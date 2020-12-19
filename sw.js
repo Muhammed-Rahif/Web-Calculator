cb990d6af9f1bd777e36c70cf0da5bedcdcf389b
Muhammed Rahif <rahifpalliyalil@gmail.com>
2020-12-19 13:11:05 +0530

    Updated file because of bug

--- sw.js
+++ sw.js
@@ -9,7 +9,14 @@ self.addEventListener('install', function(event) {
                 "./app.js",
                 "./script.js",
                 "./images/icon64.png",
-                "./images/icon512.png"
+                "./images/icon512.png",
+                "https://code.jquery.com/jquery-3.2.1.slim.min.js",
+                "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js",
+                "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js",
+                "https://kit.fontawesome.com/4f7f0e7aa6.js",
+                "https://img.icons8.com/pastel-glyph/2x/calculator.png",
+                "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css",
+                "./manifest.json"
             ]
         );
       })

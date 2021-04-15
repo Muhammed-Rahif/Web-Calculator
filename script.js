f1e0c6d96635acc95d3569112ec4f31a5a3fda42
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-04-15 08:48:25 +0530

    Some corrections

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //if browser support service worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js',{scope:'.'});
+    navigator.serviceWorker.register('sw.js',{scope:'/'});
     console.log("Service worker found")
 }
 else{
